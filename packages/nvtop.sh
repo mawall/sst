fa9ac5d9444e02f035fb09eaf7459f6d239f7f9c
Marcus Wallbaum <marcus.wallbaum@gmail.com>
2020-04-01 15:26:12 +0100

    added for descriptive error message if nvtop installation fails

--- packages/nvtop.sh
+++ packages/nvtop.sh
@@ -12,6 +12,12 @@ install_nvtop(){
   if [ "$exit_status" -eq 0 ]; then
     cd ~ && rm -rf nvtop
   else
-    echo_red "Could not install nvtop. Check ~/nvtop for details." >&2
+    echo_red "Could not install nvtop. Check ~/nvtop for manual installation.
+
+If it errors with 'Could NOT find NVML (missing: NVML_INCLUDE_DIRS)'
+try the following commands from ~/nvtop/build instead:
+
+cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
+make && sudo make install" >&2
   fi
 }
